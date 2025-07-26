6cff2ae7e982fbaa07f4e8a2f177e17c41dd93fb
kisiaurkopuchhoyaar <gauravkumarbin0988@gmail.com>
2025-07-26 10:11:52 +0530

    Add project files.

---
+++ MinimalDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinimalDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
